49d6dddb2f8ab9db538a8ef8d9c55eed45736862
Crno <118383162+Crn0@users.noreply.github.com>
2024-08-17 15:02:04 +0800

    style(App): create index css style

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react';
+import { Outlet, useLoaderData } from 'react-router-dom';
+import ThemeContext from './context/themeContext';
+import UserContextContext from './context/userContext';
+import Header from './components/ui/header';
+import Footer from './components/ui/footer/Footer';
+import localStorage from './helpers/storage/localStorage';
+import style from './index.module.css';
 
 function App() {
-  const [theme, setTheme] = useState('light')
-  const currentTheme = theme === 'light' ? 'dark' : 'light';
+    const userData = useLoaderData();
+    const [user, setUser] = useState(userData?.user || null);
+    const [theme, setTheme] = useState(() => {
+        if (localStorage.has('theme')) return localStorage.get('theme');
 
-  return (
-    <div className= {`${currentTheme}`} style={{display: 'grid', placeContent: 'center', height: '100dvh'}}>
-     <header>
-        <button onClick={() => setTheme((t) => t === 'light' ? 'dark' : 'light')}>
-          { theme === 'light' ? 'dark mode ': 'light mode' }
-         </button>
-     </header>
-      
-    </div>
-  )
+        return 'dark';
+    });
+    const themeMemo = useMemo(() => ({ theme, setTheme }), [theme, setTheme]);
+    const userMemo = useMemo(() => ({ user, setUser }), [user, setUser]);
+
+    return (
+        <div className={`${style.app} ${theme === 'dark' ? style.dark : style.light}`}>
+            <ThemeContext.Provider value={themeMemo}>
+                <UserContextContext.Provider value={userMemo}>
+                    <Header setTheme={setTheme} />
+
+                    <main className={`${theme}`}>
+                        <Outlet />
+                    </main>
+
+                    <Footer />
+                </UserContextContext.Provider>
+            </ThemeContext.Provider>
+        </div>
+    );
 }
 
-export default App
+export default App;
