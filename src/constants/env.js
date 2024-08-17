9758cb215aaca043b1d59c540625995e2e4918ff
Crno <118383162+Crn0@users.noreply.github.com>
2024-08-17 14:58:54 +0800

    feat: constant variables

---
+++ src/constants/env.js
@@ -0,0 +1,4 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const TINY_MCE_KEY = import.meta.env.VITE_TINY_MCE_KEY;
+
+export { BASE_URL, TINY_MCE_KEY };
---
+++ src/constants/form.js
@@ -0,0 +1,17 @@
+const FIRST_NAME = 'firstName';
+const LAST_NAME = 'lastName';
+const EMAIL = 'email';
+const USERNAME = 'username';
+const PWD = 'password';
+const CONFIRM_PWD = 'confirm_password';
+const BODY = 'body';
+
+export default {
+    FIRST_NAME,
+    LAST_NAME,
+    EMAIL,
+    USERNAME,
+    PWD,
+    CONFIRM_PWD,
+    BODY,
+};
