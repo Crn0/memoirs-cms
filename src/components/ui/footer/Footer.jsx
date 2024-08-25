f7d5e13aae4eefa86afb24de053d1a3b3fa19d2f
Crno <118383162+Crn0@users.noreply.github.com>
2024-08-25 13:28:03 +0800

    style: add style to footer component

--- src/components/ui/footer/Footer.jsx
+++ src/components/ui/footer/Footer.jsx
@@ -1,18 +1,24 @@
 import { useContext } from 'react';
 import { FaGithub } from 'react-icons/fa';
 import ThemeContext from '../../../context/themeContext';
+import style from './css/footer.module.css';
+import currentTheme from '../../../helpers/theme/currentTheme';
 
 export default function Footer() {
     const { theme } = useContext(ThemeContext);
 
+    const currTheme = currentTheme(theme);
+
     return (
-        <footer style={{ marginTop: 'auto' }} className={`${theme}`}>
-            Crn0
+        <footer
+            className={`${style.footer} ${currTheme(style['footer--light'], style['footer--dark'])}`}
+        >
+            <p>Crn0</p>
             <a
-                href='https://github.com/Crn0/memoirs'
-                aria-label='link'
-                target='_blank'
-                rel='noopener noreferrer'
+                href="https://github.com/Crn0/memoirs"
+                aria-label="link"
+                target="_blank"
+                rel="noopener noreferrer"
             >
                 <FaGithub />
             </a>
