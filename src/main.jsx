954bb214b0b32e9f674af07a1dc13904e1d18bdc
Crno <118383162+Crn0@users.noreply.github.com>
2024-08-25 13:11:09 +0800

    style: define global css

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import Router from './routes/index';
 import './global-styles/index.css';
 import './global-styles/reset.css';
 import './global-styles/theme.css';
+import './global-styles/buttonTheme.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
