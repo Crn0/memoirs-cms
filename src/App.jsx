170efc20b84568fbd181a335b3878f2b108fa2b7
Crno <118383162+Crn0@users.noreply.github.com>
2024-08-28 14:18:43 +0800

    feat: logout user if they did not have permission to view this site

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
-import { useEffect, useMemo, useState } from 'react';
-import { Outlet, useLoaderData } from 'react-router-dom';
+import { useContext, useEffect, useMemo, useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import ThemeContext from './context/themeContext';
-import UserContextContext from './context/userContext';
 import Header from './components/ui/header';
 import Footer from './components/ui/footer/Footer';
-import localStorage from './helpers/storage/localStorage';
 import style from './index.module.css';
 
 function App() {
-    const userData = useLoaderData();
-    const [user, setUser] = useState(userData?.user || null);
-    const [theme, setTheme] = useState(() => {
-        if (localStorage.has('theme')) return localStorage.get('theme');
-
-        return 'dark';
-    });
-    const themeMemo = useMemo(() => ({ theme, setTheme }), [theme, setTheme]);
-    const userMemo = useMemo(() => ({ user, setUser }), [user, setUser]);
-
-    useEffect(() => {
-        document.body.style.backgroundColor = theme === 'light' ? '#F5F5F5' : 'black';
-    }, [theme]);
+    const { theme, setTheme } = useContext(ThemeContext)
+    
 
     return (
         <div className={`${style.app} ${theme === 'dark' ? style.dark : style.light}`}>
-            <ThemeContext.Provider value={themeMemo}>
-                <UserContextContext.Provider value={userMemo}>
                     <Header setTheme={setTheme} />
 
                     <main className={`${style.main}`}>
@@ -33,8 +18,6 @@ function App() {
                     </main>
 
                     <Footer />
-                </UserContextContext.Provider>
-            </ThemeContext.Provider>
         </div>
     );
 }
--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,14 +1,43 @@
 import PropTypes from 'prop-types';
-import { Navigate } from 'react-router-dom';
+import { useEffect, useMemo, useState } from 'react';
+import { Navigate, useLoaderData } from 'react-router-dom';
+import ThemeContext from '../context/themeContext';
+import UserContextContext from '../context/userContext';
 import localStorage from '../helpers/storage/localStorage';
 
 function ProtectedRoute({ children }) {
-    const isLogin = localStorage.has('token');
+    const userData = useLoaderData();
+    const [user, setUser] = useState(userData?.user || null);
+    const [theme, setTheme] = useState(() => {
+        if (localStorage.has('theme')) return localStorage.get('theme');
+
+        return 'dark';
+    });
+
+    const userMemo = useMemo(() => ({ user, setUser }), [user, setUser]);
+    const themeMemo = useMemo(() => ({ theme, setTheme }), [theme, setTheme]);
+    
+    useEffect(() => {
+        document.body.style.backgroundColor = theme === 'light' ? '#F5F5F5' : 'black';
+        if (!['Admin', 'Author'].includes(user.membership)) {
+            localStorage.remove('token');
+        }
+    }, [theme,user]);
+
 
     return (
         <>
             {(() => {
-                if (isLogin) return children;
+                if (['Admin', 'Author'].includes(user.membership)) return (
+                <ThemeContext.Provider value={themeMemo}>
+
+                    <UserContextContext.Provider value={userMemo}>
+                        {
+                            children
+                        }
+                    </UserContextContext.Provider>
+                </ThemeContext.Provider>
+                );
 
                 return <Navigate to='/login' replace />;
             })()}
