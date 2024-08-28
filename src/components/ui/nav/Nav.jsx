c3bf6ebfbabe3ab5f967f62dfd7919475c74b353
Crno <118383162+Crn0@users.noreply.github.com>
2024-08-28 18:41:06 +0800

    refactor: change link name

--- src/components/ui/nav/Nav.jsx
+++ src/components/ui/nav/Nav.jsx
@@ -43,7 +43,7 @@ export default function NavBar() {
                                     className={theme}
                                 >
                                     {' '}
-                                    New post{' '}
+                                    Post{' '}
                                 </Link>
                             </div>
 
