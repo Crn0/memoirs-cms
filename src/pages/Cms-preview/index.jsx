9fddb4da7f2d4df306afa93f55498ec01d7345be
Crno <118383162+Crn0@users.noreply.github.com>
2024-08-28 20:32:47 +0800

    feat: add theme switcher

--- src/pages/Cms-preview/index.jsx
+++ src/pages/Cms-preview/index.jsx
@@ -1,18 +1,33 @@
-import { useReducer, useState } from 'react';
+import { useEffect, useMemo, useReducer, useState } from 'react';
+import ThemeContext from '../../context/themeContext';
 import Header from '../../components/ui/header/index';
 import Preview from './Preview';
 import BlogForm from './Form';
 import Button from '../../components/ui/button/Button';
 import Footer from '../../components/ui/footer/Footer';
 import reducer, { formState } from './reducer';
+import localStorage from '../../helpers/storage/localStorage';
 import style from './css/index.module.css';
 
 export default function CMSPreview() {
     const [formData, dispatch] = useReducer(reducer, formState);
     const [preview, setPreview] = useState(false);
+    const [theme, setTheme] = useState(() => {
+        if (localStorage.has('theme')) return localStorage.get('theme');
+
+        return 'dark';
+    });
+
+    const themeMemo = useMemo(() => ({ theme, setTheme }), [theme, setTheme]);
+    
+    useEffect(() => {
+        document.body.style.backgroundColor = theme === 'light' ? '#F5F5F5' : 'black';
+        
+    }, [theme]);
 
     return (
-        <div className={`${style.app}`}>
+        <ThemeContext.Provider value={themeMemo}>
+            <div className={`${style.app}`}>
     <Header />
             <main>
             <section>
@@ -53,5 +68,6 @@ export default function CMSPreview() {
 
             <Footer />
         </div>
+        </ThemeContext.Provider>
     );
 }
